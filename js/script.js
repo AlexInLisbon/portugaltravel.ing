8a094b70ea1f62f37d2c80f1317f9826de0ac6b0
AlexInLisbon <125911446+AlexInLisbon@users.noreply.github.com>
2024-02-20 19:26:50 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+// burger menu
+const icon = document.querySelector('.icon-menu');
+icon.addEventListener('click', function () {
+	document.documentElement.classList.toggle('menu-open');
+});
+// burger menu
+
+
+
